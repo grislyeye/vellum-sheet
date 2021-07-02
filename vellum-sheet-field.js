47bfc81390fdc22c8f80e0d3cffc8f1133e8e9e6
R.G. Wood <ric@grisleyeye.com>
2021-07-02 16:02:52 +0100

    Add: fields are editable

--- vellum-sheet-field.js
+++ vellum-sheet-field.js
@@ -9,14 +9,15 @@ class SheetField extends LitElement {
     .content {
       display: flex;
       flex-direction: column;
-      justify-content: flex-end;
+      height: 100%;
+      width: 100%;
     }
 
     p {
       font-size: 2em;
       padding: 0;
       margin: 0;
-      text-align: left;
+      text-align: inherit;
       border: 0;
       border-bottom: 1px black solid;
     }
@@ -27,14 +28,56 @@ class SheetField extends LitElement {
     }
 
     h2 {
-      font-size: 0.8em;
+      font-size: 12px;
       text-transform: uppercase;
       padding: 0.2em 0 0 0;
       margin: 0;
       color: black;
       font-weight: normal;
-    }`
+    }
 
+    input[type="text"] {
+      font-size: 2em;
+      font-family: inherit;
+      background: transparent;
+      border: none;
+      padding: 0;
+      margin: 0;
+      border: 0;
+      border-bottom: 1px black solid;
+      height: 100%;
+      width: 100%;
+      text-align: inherit;
+    }
+
+    :host(.small) input[type="text"] {
+      min-height: 1.2em;
+      font-size: 1.2em;
+    }
+
+    :host([class~="horizontal"]) .content {
+      display: flex;
+      flex-direction: row;
+    }
+
+    :host([class~="horizontal"]) .content > * {
+      margin-top: auto;
+      margin-bottom: auto;
+    }
+
+    :host([class~="horizontal"]) input[type="text"],
+    :host([class~="horizontal"]) p {
+      border: 0;
+      border-right: 1px black solid;
+      text-align: center;
+      width: var(--vellum-sheet-input-width, 50%);
+      font-size: inherit;
+    }
+
+    :host([class~="horizontal"]) h2 {
+      padding-left: 0.5em;
+      font-size: 12px;
+    }`
   }
 
   static get properties() {
@@ -44,6 +87,9 @@ class SheetField extends LitElement {
       },
       lines: {
         type: Number
+      },
+      editable: {
+        type: Boolean
       }
     }
   }
@@ -51,12 +97,13 @@ class SheetField extends LitElement {
   constructor() {
     super()
     this.lines = 1
+    this.editable = false
   }
 
   render() {
     return html`
     <div class="content">
-      <p><slot></slot></p>
+      ${this.editable ? html`<input type="text">` : html`<p><slot></slot></p>`}
       ${this.lines > 1 ? [...Array(this.lines - 1)].map(a => html`<p>&nbsp;</p>`) : html``}
       <h2>${this.label}</h2>
     </div>`
